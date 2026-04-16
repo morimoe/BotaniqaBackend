62bd4f222e24dad783c27eea25295581d39a6cf4
morimoe <morimoeee@gmail.com>
2026-04-16 21:30:28 +0300

    fix warnings

--- Botaniqa.Domain/Entities/User/ULoginData.cs
+++ Botaniqa.Domain/Entities/User/ULoginData.cs
@@ -1,7 +1,7 @@
 ﻿public class ULoginData
 {
-    public string Credential {  get; set; }
-    public string Password { get; set; }
-    public string LoginIp { get; set; }
+    public string Credential {  get; set; } = string.Empty;
+    public string Password { get; set; } = string.Empty;
+    public string LoginIp { get; set; } = string.Empty;
     public DateTime LoginDateTime { get; set; }
 }
--- Botaniqa.Domain/Entities/User/UserLogin.cs
+++ Botaniqa.Domain/Entities/User/UserLogin.cs
@@ -4,11 +4,11 @@ public class UserLogin
 {
     [Required]
     [Display(Name = "Username or Email")]
-    public string Credential { get; set; }
+    public string Credential { get; set; } = string.Empty;
 
     [Required]
     [DataType(DataType.Password)]
     [Display(Name = "Password")]
-    public string Password { get; set; }
+    public string Password { get; set; } = string.Empty;
 
 }
