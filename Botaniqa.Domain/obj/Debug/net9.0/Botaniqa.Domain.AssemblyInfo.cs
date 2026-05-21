671ca874f0f29d761d92509e5a0478a3e863e9a8
morimoe <161158203+morimoe@users.noreply.github.com>
2026-05-21 16:56:38 +0300

    Resolve merge conflicts

--- Botaniqa.Domain/obj/Debug/net9.0/Botaniqa.Domain.AssemblyInfo.cs
+++ Botaniqa.Domain/obj/Debug/net9.0/Botaniqa.Domain.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Botaniqa.Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2d10287ac2602c0ac668e8a93ff22210650a1a74")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a6845a1b0d8f38e37f1077a1a695f899a40d19af")]
 [assembly: System.Reflection.AssemblyProductAttribute("Botaniqa.Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Botaniqa.Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
