0a1b02067679622b7e675d25bd8798cb5efb40a3
morimoe <161158203+morimoe@users.noreply.github.com>
2026-05-21 17:19:48 +0300

    added products from db

--- Botaniqa.Domain/obj/Debug/net9.0/Botaniqa.Domain.AssemblyInfo.cs
+++ Botaniqa.Domain/obj/Debug/net9.0/Botaniqa.Domain.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Botaniqa.Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a6845a1b0d8f38e37f1077a1a695f899a40d19af")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+671ca874f0f29d761d92509e5a0478a3e863e9a8")]
 [assembly: System.Reflection.AssemblyProductAttribute("Botaniqa.Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Botaniqa.Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
